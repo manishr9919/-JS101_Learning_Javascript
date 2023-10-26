8d6d4876bb34c1f3c187c10b23d493ae7da00e4c
manishr9919 <manishr9919@gamil.com>
2023-10-26 23:54:41 +0530

    file add

---
+++ variable.js
@@ -0,0 +1,25 @@
+// student infromation
+var name = "manish kumar rauniya";
+var C = "10th";
+var school = " kdm public school";
+var section = " B";
+var rollno = 25;
+// mark in 3 subject
+var subject1 = " math";
+var marks1 = 85;
+var subject2 = "hindi";
+var marks2 = 65;
+var subject3 = "english";
+var marks3 = 90; 
+
+// calculate total marks average
+// var totalmarks = marks1 + marks2+ maarks3;
+// var averagemarks = totalmarks/3;
+  // print the report chard
+console.log("-----------------Report card--------------------");
+console.log( "name      "+ "                school       "+    "                class     "+  "subject     "+"marks    ")
+console.log  (name +"      "+school+"          "+ C+"       "+subject1+ "           "+marks1      )
+console.log("                                                                  " +subject2+"         "+marks2)
+console.log("                                                                  "+subject3+"        "+marks3)
+
+              
